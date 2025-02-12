acb4ecb357222f768fb5be8c694e436db8fbe962
Diogotb <digogo@gmail.com>
2025-02-12 11:28:19 -0300

    12.02.25

---
+++ IntroducaoJs/script01.js
@@ -0,0 +1,30 @@
+//Estrutura de Dados
+
+// Condicional (if else, switch case)
+var precoProduto = 150;
+
+if (precoProduto>=100){
+    console.log("Valor a Pagar: "
+        +(precoProduto*0.9)); //10% de Desconto
+} else{
+    console.log("Valor a Pagar: "
+        +precoProduto);
+}
+
+//switch case
+var mes = 2;
+
+switch (mes) {
+    case 1:
+        console.log("Janeiro")
+        break;
+    case 2:
+        console.log("Fevereiro");
+        break;
+    case 3:
+        console.log("Março");
+        break;
+    default:
+        console.log("Outro Mês");
+        break;
+}
