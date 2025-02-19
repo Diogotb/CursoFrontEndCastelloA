69b877dcffd280e7f443b504c2aeaf287f0c864b
Diogotb <digogo@gmail.com>
2025-02-19 11:27:57 -0300

    19.02.25

---
+++ FuncoesMathDataHoraString/script00.js
@@ -0,0 +1,9 @@
+//Estudo de Funções Matemáticas (Math)
+
+//Funções Raiz e Potência (sqrt // pow)
+//raiz quadrade de 25
+console.log(Math.sqrt(25)); //5
+
+//potência
+console.log(Math.pow(3,2)); //3² = 9
+console.log(Math.pow(4,3)); //4³ = 64
